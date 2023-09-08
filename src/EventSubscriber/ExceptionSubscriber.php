7a359bc5c80d7209d0f09cfc627d7ae574585c24
Céline Foucart <celinefoucart@yahoo.fr>
2023-09-08 09:38:40 +0200

    add: error subscriber

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $statusCode = ($exception instanceof HttpException) ? $exception->getStatusCode() : 500;
+        $data = ['status' => $statusCode,'message' => $exception->getMessage()];
+        $event->setResponse(new JsonResponse($data));
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
